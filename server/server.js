7da5ae505a129c4ff7fd690753e26a35afc04f08
Mohan <125137921+Mohan1733@users.noreply.github.com>
2025-08-16 20:40:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://employee-management-system-pi-tan.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
